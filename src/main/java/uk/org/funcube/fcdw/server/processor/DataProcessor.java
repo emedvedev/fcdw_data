f15325b3b74ffea4a38e41c036aa8c7fd8183712
David Johnson <dave@g4dpz.me.uk>
2014-02-12 21:59:41 +0000

    WE now check for zero values to add to minmax

--- src/main/java/uk/org/funcube/fcdw/server/processor/DataProcessor.java
+++ src/main/java/uk/org/funcube/fcdw/server/processor/DataProcessor.java
@@ -371,7 +371,7 @@ public class DataProcessor {
 			
 			switch (channel) {
 			default:
-				if (channelValue == null || channelValue == 0) {
+				if (channelValue == null) {
 					break;
 				}
 				if (channelValue < minMaxEntity.getMinimum()) {
