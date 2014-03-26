8bb263f508c4d8de07258e1c1ff5b7703c1e93c4
David Johnson <dave@g4dpz.me.uk>
2014-03-26 10:45:38 +0000

    Added dirty flag to reduce the amount of DB access when updating MinMax

--- src/main/java/uk/org/funcube/fcdw/server/processor/DataProcessor.java
+++ src/main/java/uk/org/funcube/fcdw/server/processor/DataProcessor.java
@@ -55,9 +55,10 @@ import uk.org.funcube.fcdw.server.util.UTCClock;
 @Service
 @RequestMapping("/api/data/hex")
 public class DataProcessor {
-	
-	private static final Buffer FIFO = BufferUtils.synchronizedBuffer(new CircularFifoBuffer(1000));
-	
+
+	private static final Buffer FIFO = BufferUtils
+			.synchronizedBuffer(new CircularFifoBuffer(1000));
+
 	long TWO_DAYS_SEQ_COUNT = 1440;
 
 	@Autowired
@@ -74,7 +75,7 @@ public class DataProcessor {
 
 	@Autowired
 	Clock clock;
-	
+
 	@Autowired
 	EpochDao epochDao;
 
@@ -129,13 +130,13 @@ public class DataProcessor {
 									.equals(calculatedDigestUTF16))) {
 
 					hexString = StringUtils.deleteWhitespace(hexString);
-					
+
 					final Date now = clock.currentDate();
-					
-					FIFO.add(new UserHexString(user, StringUtils.deleteWhitespace(hexString), now));
-					//LOG.debug("Writing fifo, size:" + FIFO.size() + " , element count: " + FIFO.toArray().length);
 
-
+					FIFO.add(new UserHexString(user, StringUtils
+							.deleteWhitespace(hexString), now));
+					// LOG.debug("Writing fifo, size:" + FIFO.size() +
+					// " , element count: " + FIFO.toArray().length);
 
 					return new ResponseEntity<String>("OK", HttpStatus.OK);
 				} else {
@@ -157,59 +158,61 @@ public class DataProcessor {
 					HttpStatus.UNAUTHORIZED);
 		}
 	}
-	
+
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
 	public void processHexFrame() {
-		
+
 		Map<Long, EpochEntity> epochMap = new HashMap<Long, EpochEntity>();
-		
+
 		Iterator<EpochEntity> iterator = epochDao.findAll().iterator();
-		
+
 		while (iterator.hasNext()) {
 			EpochEntity epoch = iterator.next();
 			epochMap.put(epoch.getSatelliteId(), epoch);
 		}
-		
-		//LOG.debug("Reading fifo, size:" + FIFO.size() + " , element count: " + FIFO.toArray().length);
+
+		// LOG.debug("Reading fifo, size:" + FIFO.size() + " , element count: "
+		// + FIFO.toArray().length);
 		if (!FIFO.isEmpty()) {
 			final Iterator<UserHexString> iter = FIFO.iterator();
-			while (iter.hasNext()){
-				final UserHexString userHexString = (UserHexString)iter.next();
+			while (iter.hasNext()) {
+				final UserHexString userHexString = (UserHexString) iter.next();
 				iter.remove();
-				
+
 				final String hexString = userHexString.getHexString();
 				final UserEntity user = userHexString.getUser();
 				final Date createdDate = userHexString.getCreatedDate();
-				
-				final int frameId = Integer.parseInt(
-				hexString.substring(0, 2), 16);
-				
+
+				final int frameId = Integer.parseInt(hexString.substring(0, 2),
+						16);
+
 				final int frameType = frameId & 63;
 				final int satelliteId = (frameId & (128 + 64)) >> 6;
 				final int sensorId = frameId % 2;
 				final String binaryString = convertHexBytePairToBinary(hexString
 						.substring(2, hexString.length()));
 				final Date now = clock.currentDate();
-				final RealTime realTime = new RealTime(satelliteId,
-						frameType, sensorId, now, binaryString);
+				final RealTime realTime = new RealTime(satelliteId, frameType,
+						sensorId, now, binaryString);
 				final long sequenceNumber = realTime.getSequenceNumber();
-		
+
 				if (sequenceNumber != -1) {
-					
-					Long maxSequenceNumber = hexFrameDao.getMaxSequenceNumber(satelliteId);
-					
-					
-					if (maxSequenceNumber != null && Math.abs(sequenceNumber - maxSequenceNumber) > TWO_DAYS_SEQ_COUNT) {
+
+					Long maxSequenceNumber = hexFrameDao
+							.getMaxSequenceNumber(satelliteId);
+
+					if (maxSequenceNumber != null
+							&& Math.abs(sequenceNumber - maxSequenceNumber) > TWO_DAYS_SEQ_COUNT) {
 						LOG.error(String
 								.format("Sequence number %d is out of bounds for satelliteId %d",
 										sequenceNumber, satelliteId));
 						return;
 					}
-					
+
 					final List<HexFrameEntity> frames = hexFrameDao
 							.findBySatelliteIdAndSequenceNumberAndFrameType(
 									satelliteId, sequenceNumber, frameType);
-		
+
 					if (frames != null) {
 						saveUpdateHexFrame(hexString, user, frameType,
 								satelliteId, now, realTime, sequenceNumber,
@@ -224,34 +227,31 @@ public class DataProcessor {
 	private void saveUpdateHexFrame(final String hexString,
 			final UserEntity user, final int frameType, final int satelliteId,
 			final Date now, final RealTime realTime, final long sequenceNumber,
-			final List<HexFrameEntity> frames,
-			final EpochEntity epoch) {
+			final List<HexFrameEntity> frames, final EpochEntity epoch) {
 		HexFrameEntity hexFrame;
 		Set<UserEntity> users;
 		if (frames.size() == 0) {
-			
+
 			Timestamp satelliteTime;
-			
+
 			if (epoch == null) {
 				satelliteTime = new Timestamp(now.getTime());
 			} else {
 				satelliteTime = new Timestamp(
-					epoch.getReferenceTime().getTime() +
-					((sequenceNumber - epoch.getSequenceNumber()) * 2 * 60 * 1000) +
-					(frameType * 5 * 1000));
+						epoch.getReferenceTime().getTime()
+								+ ((sequenceNumber - epoch.getSequenceNumber()) * 2 * 60 * 1000)
+								+ (frameType * 5 * 1000));
 			}
 
-			hexFrame = new HexFrameEntity(
-					(long) satelliteId, (long) frameType,
-					sequenceNumber, hexString, now, true,
-					satelliteTime);
-			
+			hexFrame = new HexFrameEntity((long) satelliteId, (long) frameType,
+					sequenceNumber, hexString, now, true, satelliteTime);
+
 			users = hexFrame.getUsers();
 
 			users.add(user);
 
-			RealTimeEntity realTimeEntity = new RealTimeEntity(
-					realTime, satelliteTime);
+			RealTimeEntity realTimeEntity = new RealTimeEntity(realTime,
+					satelliteTime);
 
 			hexFrameDao.save(hexFrame);
 
@@ -261,7 +261,7 @@ public class DataProcessor {
 
 		} else {
 			hexFrame = frames.get(0);
-			
+
 			users = hexFrame.getUsers();
 
 			users.add(user);
@@ -344,31 +344,38 @@ public class DataProcessor {
 
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	private void checkMinMax(long satelliteId, RealTimeEntity realTimeEntity) {
-		
+
 		final Long[] longValues = realTimeEntity.getLongValues();
 
 		for (int channel = 1; channel <= 43; channel++) {
+
+			boolean isDirty = false;
+
 			List<MinMaxEntity> channels = minMaxDao
 					.findBySatelliteIdAndChannel(satelliteId, channel);
 			if (channels.isEmpty()) {
+				LOG.error("Did not find any minmax data for channel: "
+						+ channel);
 				break;
 			}
 			MinMaxEntity minMaxEntity = channels.get(0);
-			
+
 			Date refDate = minMaxEntity.getRefDate();
-			
+
 			Date currentDate = clock.currentDate();
-			
+
 			long ageInMillis = currentDate.getTime() - refDate.getTime();
-			
+
 			if (ageInMillis > 7 * 24 * 60 * 60 * 1000) {
 				minMaxEntity.setEnabled(false);
 				minMaxDao.save(minMaxEntity);
-				minMaxEntity = new MinMaxEntity(satelliteId, (long) channel, 99999L, -99999L, currentDate, true);
+				minMaxEntity = new MinMaxEntity(satelliteId, (long) channel,
+						99999L, -99999L, currentDate, true);
+				isDirty = true;
 			}
-			
+
 			Long channelValue = longValues[channel - 1];
-			
+
 			switch (channel) {
 			default:
 				if (channelValue == null) {
@@ -376,8 +383,10 @@ public class DataProcessor {
 				}
 				if (channelValue < minMaxEntity.getMinimum()) {
 					minMaxEntity.setMinimum(channelValue);
+					isDirty = true;
 				} else if (channelValue > minMaxEntity.getMaximum()) {
 					minMaxEntity.setMaximum(channelValue);
+					isDirty = true;
 				}
 				break;
 			case 9:
@@ -388,19 +397,24 @@ public class DataProcessor {
 				if (channelValue == null || channelValue == 0) {
 					break;
 				}
-				
+
 				if (channelValue >= 128) {
 					channelValue = ~channelValue ^ 255;
 				}
-				
+
 				if (channelValue < minMaxEntity.getMinimum()) {
 					minMaxEntity.setMinimum(channelValue);
+					isDirty = true;
 				} else if (channelValue > minMaxEntity.getMaximum()) {
 					minMaxEntity.setMaximum(channelValue);
+					isDirty = true;
 				}
 				break;
 			}
-			minMaxDao.save(minMaxEntity);
+			
+			if (isDirty) {
+				minMaxDao.save(minMaxEntity);
+			}
 		}
 
 	}
